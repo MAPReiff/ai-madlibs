f99e93f6e5be559baa09d11f0aad6d0cbad0467f
Mitchell <me@mreiff.dev>
2023-04-15 10:57:29 -0400

    API key properties file and read

--- ai-madlibs/src/main/java/ai/madlibs/App.java
+++ ai-madlibs/src/main/java/ai/madlibs/App.java
@@ -1,13 +1,29 @@
 package ai.madlibs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  // public static void main( String[] args )
+  // {
+  //     System.out.println( "Hello World!" );
+  // }
+
+  public static void main(String[] args) throws IOException {
+    Properties props = new Properties();
+    InputStream inputStream =
+        App.class.getClassLoader().getResourceAsStream("api.properties");
+
+    if (inputStream != null) {
+      props.load(inputStream);
     }
+    final String apiKey = props.getProperty("openAIkey");
+
+    System.out.println(apiKey);
+  }
 }
