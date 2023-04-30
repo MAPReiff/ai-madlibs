66600d602e732f5ea490432a4c832978cd3e53c1
jgerega107 <jakegerega107@gmail.com>
2023-04-30 19:12:31 -0400

    Added formatting

--- ai-madlibs/src/main/java/ai/madlibs/App.java
+++ ai-madlibs/src/main/java/ai/madlibs/App.java
@@ -1,6 +1,5 @@
 package ai.madlibs;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,16 +12,11 @@ import com.lilittlecat.chatgpt.offical.ChatGPT;
  *
  */
 public class App {
-  // public static void main( String[] args )
-  // {
-  //     System.out.println( "Hello World!" );
-  // }
 
   public static void main(String[] args) throws IOException {
     // read in API key for OpenAI from properties file
     Properties props = new Properties();
-    InputStream inputStream =
-        App.class.getClassLoader().getResourceAsStream("api.properties");
+    InputStream inputStream = App.class.getClassLoader().getResourceAsStream("api.properties");
 
     if (inputStream != null) {
       props.load(inputStream);
@@ -35,27 +29,27 @@ public class App {
     System.out.println("Enter a sentence for the AI to complete: ");
     String input = scanner.nextLine();
 
-    String prompt = chatGPT.ask("Create a fill in the blanks madlibs with type of speech from this: " + input);
-    System.out.println(prompt); // will be "\n\nHello! How may I assist you today?"
+    String prompt = chatGPT.ask(
+        "Create a fill in the blanks madlibs with type of speech from this, with each type of speech surrounded by parenthesis: "
+            + input);
+    System.out.println("\n--------------------------------------\n");
 
     String array[] = prompt.split(" ");
-    for(int i = 0; i < array.length; i++){
-      if (array[i].contains("(") && array[i].contains(")"))
-      {
-        System.out.println("Enter a word to replace: " + array[i]);
+    for (int i = 0; i < array.length; i++) {
+      if (array[i].contains("(") && array[i].contains(")")) {
+        System.out.println("\n\nEnter a word to replace: " + array[i]);
         array[i] = scanner.nextLine();
       }
-
-      System.out.println(array[i]);
+      System.out.print(array[i] + " ");
     }
 
     System.out.println("Your madlib is: ");
     String madlib = "";
-    for(int i = 0; i < array.length; i++){
+    for (int i = 0; i < array.length; i++) {
       madlib += array[i] + " ";
     }
-    
-      System.out.println(madlib);
+
+    System.out.println(madlib);
 
   }
 }
